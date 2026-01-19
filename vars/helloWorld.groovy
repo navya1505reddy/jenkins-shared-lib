713416ef458ea5a56e62884e90c6d7313db975ee
Navya bollareddy <bollareddynavya@gmail.com>
2026-01-19 18:41:17 +0530

    Implement helloWorld function in shared library
    Add a function to print job details and branch info

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,1 +1,9 @@
 
+def call() {
+    echo "Hello World from Shared Library"
+    echo "Job Name: ${env.JOB_NAME}"
+    echo "Build Number: ${env.BUILD_NUMBER}"
+    if (env.GIT_BRANCH) {
+        echo "Branch: ${env.GIT_BRANCH}"
+    }
+}
